d7700b9b5007ee4d21383f1ca28d2d1f8b4bea62
pabloramsj <159783304+pabloramsj@users.noreply.github.com>
2025-11-03 20:36:15 +0100

    Update Entrenamiento red.py

--- Entrenamiento red.py
+++ Entrenamiento red.py
@@ -296,7 +296,7 @@ def train_stage(stage_name, train_bs, lr, epochs,
             best_state = {k: v.detach().cpu().clone() for k, v in model.state_dict().items()}
             # checkpoint inmediato del mejor
             if ckpt_path:
-                save_ckpt(ckpt_path, model, optimizer, scaler, epoch, best_val,
+                save_ckpt(ckpt_path, epoch, model, optimizer, scaler, best_val,
                           stage_meta={"stage": stage_name, "lr": lr,
                                      "train_bs": train_bs, "physical_bs": physical_bs})
 
@@ -358,4 +358,5 @@ torch.save(model.state_dict(), "mlp_rde_two_stage.pt")
 print("Modelo guardado en 'mlp_rde_two_stage.pt'")
 
 end = time.time()     # Marca final
+
 print(f"Tiempo de ejecución: {end - start:.3f} segundos")
